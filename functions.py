b733253e2ff59626d45e6aca3ad1ba8f7feb481f
Jamal El Kuweiss <jamal.elkuweiss@stud.uni-goettingen.de>
2021-07-16 00:05:54 +0200

    Replaced all the data related to the elements in the code with one list of dictionaries at the very top of the script that should contain all the required stuff.

--- functions.py
+++ functions.py
@@ -5,6 +5,26 @@ from scipy.optimize import root_scalar
 from sympy import *
 import math
 
+#List of dictionaries for element properties
+#first dictionary contains the statistical weight of each ionised state of the atom
+#Second one is the energy difference xi between the states
+#Third one is the number of ionised states of each element
+element_properties = [
+    {
+        "H" : [2,1],
+        "He": [1,2,1],
+    },
+    {
+        "H" : [13.6],
+        "He": [24.6,54.4],
+    },
+    {
+        "H" : 1,
+        "He": 2,
+    }
+]
+
+
 # Global Constants
 h =  2*math.pi
 kb = 1
@@ -26,14 +46,11 @@ def lam(T,xi,z0,z1):
 #el_dens is a list of densities [5000,6000]
 #T is the temperature
 def matrix_builder(el_names,el_dens,T):
-    #Known general data
-    el_io_levels = {'H':1, 'He':2, 'C':3}
-     
     #First we need to get the dimension of the vector in the matrix:
     #Get the total ionisation levels
     total_io_levels = 0
     for element in el_names:
-        total_io_levels = total_io_levels + el_io_levels[element]
+        total_io_levels = total_io_levels + element_properties[2][element]
     #Dimesion of rows of augmented matrix
     dim_v = 1 + len(el_names) + total_io_levels
     
@@ -41,23 +58,22 @@ def matrix_builder(el_names,el_dens,T):
     #If you take the example of Hydrogen and Helium (1 + 2 saha equations; 2 total density equations; 1 electron density equation)
     #you will find that you can order them in block matrices (all but the electron density one)
     #We will use this fact to construct the matrix with loops
-    M = []
-    row = []
-    column_skip_counter = 0
+    M = [] #The matrix
+    row = [] #Row buffer to build the matrix
+    column_skip_counter = 0 #skip counter to help us build a column matrix (placing zeroz:)
     for element in el_names:
 
-        dim_block = el_io_levels[element]+1 #size of the block matrix of each element
-        block_block_skip_counter = 0
+        dim_block = element_properties[2][element] + 1 #size of the block matrix of each element
+        block_block_skip_counter = 0 #skip counter for each element block matrix (they are mostly upper triangular)
 
         for i in range(dim_block - 1): #the -1 is because the last line of the block is just a list of 1's
-            #column skippers are my best way so far of building diagonal matrices
             column_skip_counter = column_skip_counter + 1
 
-            #first we do all the skipping we have to do
+            #first we do all the skipping we have to do to reach the place of our element block
             for k in range(0,column_skip_counter):
                 row.append(0)
 
-            #Then we build the block lines: first by skipping enough to make the block diagonal
+            #Then we build the block lines: first by skipping enough to make the block upper triangular
             for k in range(column_skip_counter,column_skip_counter+block_block_skip_counter):
                 row.append(0)
 
@@ -68,6 +84,8 @@ def matrix_builder(el_names,el_dens,T):
             #fill the rest with zeros
             for k in range(column_skip_counter+block_block_skip_counter + 2, dim_v):
                 row.append(0)
+
+            #Append to the matrix, clear the row, and do it again
             M.append(row)
             row.clear()
 

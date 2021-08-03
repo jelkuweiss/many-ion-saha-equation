562239341391180d0fb10c0644c64cb1c89de15b
Jamal El Kuweiss <jamal.elkuweiss@stud.uni-goettingen.de>
2021-08-03 16:23:09 +0200

    Minor changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import functions as saha
 
-#M = saha.augmented_matrix_builder(["H", "He"], [1662424176, 719087680], 1.549e+07)
-M = saha.augmented_matrix_builder(["H", "He","O"], [5000, 5000, 5000], 25000)
+M = saha.augmented_matrix_builder(["H", "He"], [1662424176, 719087680], 1.549e+07)
+#M = saha.augmented_matrix_builder(["H", "He","O"], [5000, 5000, 5000], 25000)
 print(M)
 s = saha.matrix_solver_for_ne(M, 3100599537)
 

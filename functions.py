c2f44e2d3001ca54c3aa14bd9fc1d1882466622f
Jamal El Kuweiss <jamal.elkuweiss@stud.uni-goettingen.de>
2021-08-04 15:54:05 +0200

    Changed the matrix builder to list algebra to clean up the code

--- functions.py
+++ functions.py
@@ -5,12 +5,12 @@ import math
 from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 
-#List of dictionaries for element properties
-#first dictionary contains the statistical weight of each ionised state of the atom
-#Second one is the energy difference xi between the states
-#Third one is the number of ionised states of each element
+# List of dictionaries for element properties
+# First dictionary contains the statistical weight of each ionised state of the atom
+# Second one is the energy difference xi between the states
+# Third one is the number of ionised states of each element
 
-element_properties = [
+el_prop = [
     {
         "H": [2, 1],
         "He": [1, 2, 1],
@@ -41,124 +41,71 @@ kb = 1
 me = 511e3 #[eV]
 
 
-#This function represents the right hand side of the saha equation
-#It will in the matrix builder to fill in for the values
-#T is the temperature, xi is the energy difference of the states
-#z0 and z1 are the statistical weights of the states (lowest and highest, respectively)
+# This function represents the right hand side of the saha equation
+# It will in the matrix builder to fill in for the values
+# T is the temperature, xi is the energy difference of the states
+# z0 and z1 are the statistical weights of the states (lowest and highest, respectively)
 # (lam means lamda, the usual symbol associated with this side of the equation)
 def lam(T, xi, z0, z1):
     return (z1/z0) * ((2*math.pi*me*kb*T)**1.5 / (h**3)) * math.exp((-xi) / (kb*T))
 
 
-#This function builds the matrix that represents the system of many equations
-#It takes in the list of elements, as well as their respective densities and the temperature in the region
-#el_names is a list of names ['H','He']
-#el_dens is a list of densities [5000,6000]
-#T is the temperature
-def augmented_matrix_builder(el_names, el_dens, T):
-    #First we need to get the dimension of the vector in the matrix:
-    #Get the total ionisation levels
+# This function builds the matrix that represents the system of many equations
+# It takes in the list of elements, as well as their respective densities and the temperature in the region
+# el_names is a list of names ['H','He']
+# el_dens is a list of densities [5000,6000]
+# T is the temperature
+def augmented_matrix_builder(el_names, el_dens, t):
+    # We need the total ionisation levels from all our elements to know the dimension of the matrix
     total_io_levels = 0
-    for element in el_names:
-        total_io_levels = total_io_levels + element_properties[2][element]
-    #Dimesion of rows of augmented matrix
-    dim_v = 1 + len(el_names) + total_io_levels
-
-    #Define the symbol of the energy density
+    for el in el_names:
+        total_io_levels += el_prop[2][el]
+    dim_h = 1 + len(el_names) + total_io_levels  # Horizontal dimension of M
     global nE
     nE = symbols('n_e')
 
-    #Now we build the Matrix:
-    #If you take the example of Hydrogen and Helium (1 + 2 saha equations; 2 total density equations; 1 electron density equation)
-    #you will find that you can order them in block matrices (all but the electron density one)
-    #We will use this fact to construct the matrix with loops
-    M = [] #The matrix
-    row = [] #Row buffer to build the matrix
-    column_skip_counter = 0 #skip counter to help us build a column matrix (placing zeroz:)
+    M = []
+    row_buffer = []
+    skip_counter_1 = 0
+    skip_counter_2 = 0
 
-    for e in range(len(el_names)):
+    for i, el in enumerate(el_names):
+        for j in range(el_prop[2][el]):
+            # Insert the saha equation line for each energy transition
+            row_buffer = (skip_counter_1) * [0] + [-lam(t, el_prop[1][el][j], el_prop[0][el][j], el_prop[0][el][j + 1]),
+                                                   nE] + (dim_h - skip_counter_1 - 2) * [0]
+            M.append(row_buffer)
+            # Skip a column next time
+            skip_counter_1 += 1
+        # Skip extra column after each element
+        skip_counter_1 += 1
 
-        element = el_names[e]
-        dim_block = element_properties[2][element] + 1 #size of the block matrix of each element
-        block_column_skip_counter = 0 #skip counter for each element block matrix (they are mostly upper triangular)
+        # Total density row
+        row_buffer = (skip_counter_2) * [0] + (el_prop[2][el] + 1) * [1] + (
+                    dim_h - 1 - skip_counter_2 - el_prop[2][el] - 1) * [0] + [el_dens[i]]
+        M.append(row_buffer)
+        skip_counter_2 += el_prop[2][el] + 1
 
-        for i in range(dim_block - 1): #the -1 is because the last line of the block is just a list of 1's
-            column_skip_counter = column_skip_counter + 1
+    # Electron Density row
+    row_buffer = []
+    for el in el_names:
+        for k in range(el_prop[2][el] + 1):
+            row_buffer += [k]
+    row_buffer += [nE]
+    M.append(row_buffer)
 
-            #first we do all the skipping we have to do to reach the place of our element block
-            # row += (column_skip_counter-1)*[0] # 5*[0] <-> [0 for i in range(5)] # [i for i in range(3)]  = [0,1,2]
-            for k in range(0,column_skip_counter-1):
-                row.append(0)
-
-            #Then we build the block lines: first by skipping enough to make the block upper triangular
-            for k in range(column_skip_counter,column_skip_counter+block_column_skip_counter-1):
-                row.append(0)
-
-            #Then we place the two elements
-            row.append(-lam(T, element_properties[1][element][i], element_properties[0][element][i], element_properties[0][element][i+1]))
-            row.append(nE)
-
-            #fill the rest with zeros
-            for k in range(column_skip_counter+block_column_skip_counter + 2 -1, dim_v-1): #-1 here since the last column is built seperately
-                row.append(0)
-
-            #Append to the matrix, clear the row, and do it again
-            M.append(row)
-            row = []
-
-        #Now the last line in the block is the total density line which is the same for all elements
-        if e > 0:
-            previous_elements_block_size = 0
-            for n in range(e):
-                previous_elements_block_size = previous_elements_block_size + element_properties[2][el_names[n]] +1
-            for k in range(0, previous_elements_block_size):
-                row.append(0)
-        for k in range(dim_block):
-            row.append(1)
-        for k in range(column_skip_counter+dim_block, dim_v):
-            row.append(0)
-        M.append(row)
-        row = []
-        column_skip_counter = column_skip_counter + 1
-
-
-    #Finally append the electron density row
-    row = []
-    for element in el_names:
-        dim_block = element_properties[2][element] + 1 #calculate it again
-        for i in range(dim_block):
-            row.append(i)
-    M.append(row)
-    row = []
-
-    #remains to create the right hand side vector containing the total densities and one electron density
-    col = []
-    basic_counter = 0
-    for element in el_names:
-        dim_block = element_properties[2][element] + 1  # calculate it again!
-        for i in range(dim_block-1):
-            col.append(0)
-        col.append(el_dens[basic_counter])
-        basic_counter = basic_counter + 1
-    col.append(nE)
-
-    #Now we add this column to the matrix @@ complete please
-    for l in range(len(M)):
-        M[l].append(col[l])
-
-    #Print the matrix (only in testing phase)@@
+    # Print the matrix (only in testing phase)@@
     for l in M:
         print(l)
     # Print the matrix lengths (only in testing phase)@@
     for l in M:
         print(len(l))
-    #Should we return a list of lists (as we do here) or a sympy matrix
+    # Should we return a list of lists (as we do here) or a sympy matrix
     return M
 
-#This function solves an augmented matrix containing an electron density symbol(sympy)
+# This function solves an augmented matrix containing an electron density symbol(sympy)
 def matrix_solver_for_ne(M,max_ne):
-
-    #First we define this function and expression to get the electron density from optimisation
+    # First we define this function and expression to get the electron density from optimisation
     M = Matrix(M)
     determ_augm = M.det()
     print("Determinant of the matrix")  # for trial runs only @@
@@ -167,23 +114,17 @@ def matrix_solver_for_ne(M,max_ne):
     # max_ne = @@ #Should we ask it from the user?
     min_ne = 0
 
-    #plot det(n_e) vs n_e in the region [0 , n_max] remember labels
+    # plot det(n_e) vs n_e in the region [0 , n_max] remember labels
     ne_values = range(min_ne, max_ne+1, round(max_ne/100))
     det_values = []
     for val in ne_values:
         det_values.append(determ_augm.subs(nE, val))
-    det_vs_ne_fig = plt.figure()
-    ax1 = det_vs_ne_fig.add_subplot(111)
-    ax1.scatter(ne_values,det_values)
-    ax1.set(title='Determinant of the Augmented Matrix vs the Electron Density',
-            xlabel='nE',
-            ylabel='det(nE)')
-    det_vs_ne_fig.show()
+    det_vs_ne_fig = plt.scatter(ne_values, det_values)
 
     print("Determinant of the matrix")  # for trial runs only @@
     print(determ_augm)  # for trial runs only @@
 
-    #Express the determinant polynomial as a function and give it to root scalar from scipy with bounds
+    # Express the determinant polynomial as a function and give it to root scalar from scipy with bounds
     def determinant_polynomial(ne):
         return determ_augm.subs(nE, ne)
     print("The interval is between")
@@ -196,7 +137,7 @@ def matrix_solver_for_ne(M,max_ne):
     print("Root of the determinant polynomial in the given interval")  # for trial runs only @@
     print(sol.root)  # for trial runs only @@
 
-    #Substitute ne for the root we found
+    # Substitute ne for the root we found
     M.subs(nE, sol.root)
     print(M)  # for trial runs only @@
 
